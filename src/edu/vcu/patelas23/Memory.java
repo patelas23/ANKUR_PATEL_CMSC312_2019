e4773fe6ced1e691f71901556c54136fe8dde98a
Ankur Patel <patelas23@vcu.edu>
2019-12-10 13:46:19 -0500

    comment outline and basic functions

---
+++ src/edu/vcu/patelas23/Memory.java
@@ -0,0 +1,17 @@
+package edu.vcu.patelas23;
+
+//Class to represent system memory, with demand paging
+//Only CPU has mutator rights
+//Process either address + read request
+//or address + data and write request
+//Address space is allocated per process, shared between threads
+public class Memory {
+    public void read(int address) {
+
+    }
+
+    public void write(int address, int data) {
+
+    }
+
+}
